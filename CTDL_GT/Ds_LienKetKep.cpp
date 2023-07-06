7a300b60e4ba378727d4954ab4bd2c404f2a732f
your name <t.t.thuong2004@gmail.com>
2023-07-06 10:18:28 +0700

    Stack_Queue

--- CTDL_GT/Ds_LienKetKep.cpp
+++ CTDL_GT/Ds_LienKetKep.cpp
@@ -1,166 +1,166 @@
-#include <iostream>
-using namespace std;
-struct Node {
-	int value;
-	Node *next;
-	Node *prev;
-};
-struct ds_Kep {
-	Node* head;
-	Node* tail;
-};
-void init(ds_Kep& k)
-{
-	k.head = NULL;
-	k.tail = NULL;
-}
-Node* createNode(int x)
-{
-	Node* p = new Node;
-	p->value = x;
-	p->next = NULL;
-	p->prev = NULL;
-	return p;
-}
-void themDau(ds_Kep& k, int x)
-{
-	Node* p = createNode(x);
-	if (k.head == NULL)
-	{
-		k.head = k.tail = p;
-	}
-	else
-	{
-		p->next = k.head;
-		k.head->prev = p;
-		k.head = p;
-	}
-}
-void themCuoi(ds_Kep& k, int x)
-{
-	Node* p = createNode(x);
-	if (k.head == NULL)
-	{
-		k.head = k.tail = p;
-	}
-	else 
-	{
-		p->prev = k.tail;
-		k.tail->next = p;
-		k.tail = p;
-	}
-}
-void themsau(ds_Kep& k, int v, int x)
-{
-	Node* q = k.head;
-	while (q != NULL && q->value != v)
-		q = q->next;
-
-	if (q != NULL)
-	{
-		Node* p = createNode(x);
-
-		p->next = q->next;
-		q->next->prev = p;
-
-		q->next = p;
-		p->prev = q;
-	}
-}
-void xoaSau(ds_Kep& k, int v)
-{
-	Node* q = k.head;
-	while (q != NULL && q->value != v)
-		q = q->next;
-
-		 
-		Node* p = q->next;
-		q->next = p->next;
-		if (p->next != NULL)
-		{
-			p->next->prev = q;
-		}
-		delete p;
-}
-void xoaDau(ds_Kep& k)
-{
-	if (k.head != NULL)
-	{
-		Node* p = k.head;
-		if (p->next == NULL)
-		{
-			k.tail = NULL;
-			k.head = NULL;
-		}
-		else
-		{
-			k.head = p->next;
-			p->next->prev = NULL;
-			p->next = NULL;
-		}
-		delete(p);
-	}
-}
-void xoaCuoi(ds_Kep& k)
-{
-	if (k.head != NULL)
-	{
-		Node* p = k.tail;
-		k.tail = p->prev;
-		p->next = NULL;
-		p->prev->next = NULL;
-
-		delete(p);
-	}
-}
-
-Node* timKiem(ds_Kep &k, int v)
-{
-	Node* p = k.head;
-	while (p != NULL && p->value != v)
-	{
-		p = p->next;
-	}
-	return p;
-	
-}
-void output(ds_Kep k)
-{
-	Node* p = k.head;
-	while (p!=NULL)
-	{
-		cout << p->value << "\t";
-		p = p->next;
-	}
-}
-void output_1(ds_Kep k)
-{
-	Node* p = k.tail;
-	while (p!=NULL)
-	{
-		cout << p->value << "\t";
-		p = p->prev;
-	}
-}
-int main()
-{
-	ds_Kep k;
-	init(k);
-	themCuoi(k, 10);
-	themDau(k, 20);
-	themDau(k, 30);
-	themCuoi(k, 50);
-	themsau(k, 30, 31); // 30 31 20 10 50
-	xoaDau(k); // 31 20 10 50
-	xoaCuoi(k); // 31 20 10
-	xoaSau(k, 10); // 31 20s
-	output(k);
-	cout << endl;
-	if (timKiem(k, 110) != NULL)
-		cout << "Tim Thay";
-	else
-	{
-		cout << "Khong tim thay";
-	}
-	return 0;
-}
+//#include <iostream>
+//using namespace std;
+//struct Node {
+//	int value;
+//	Node *next;
+//	Node *prev;
+//};
+//struct ds_Kep {
+//	Node* head;
+//	Node* tail;
+//};
+//void init(ds_Kep& k)
+//{
+//	k.head = NULL;
+//	k.tail = NULL;
+//}
+//Node* createNode(int x)
+//{
+//	Node* p = new Node;
+//	p->value = x;
+//	p->next = NULL;
+//	p->prev = NULL;
+//	return p;
+//}
+//void themDau(ds_Kep& k, int x)
+//{
+//	Node* p = createNode(x);
+//	if (k.head == NULL) // danh sach rong
+//	{
+//		k.head = k.tail = p;
+//	}
+//	else
+//	{
+//		p->next = k.head; // gan gia tri dia chi bo nho cua head cho vung next cua p
+//		k.head->prev = p; // dieu chinh prev cua head thanh p
+//		k.head = p;       // cap nhat p la head
+//	}
+//}
+//void themCuoi(ds_Kep& k, int x)
+//{
+//	Node* p = createNode(x);
+//	if (k.head == NULL) // danh sach rong
+//	{
+//		k.head = k.tail = p; 
+//	}
+//	else 
+//	{
+//		p->prev = k.tail; // chinh gia tri vung lien ket prev cua p thanh phan tu cuoi cung
+//		k.tail->next = p; // gan gia tri dia chi bo nho cua p cho vung tiep theo	
+//		k.tail = p;		  // cap nhat p la last
+//	}
+//}
+//void themsau(ds_Kep& k, int v, int x)
+//{
+//	Node* q = k.head;
+//	while (q != NULL && q->value != v)
+//		q = q->next;
+//
+//	if (q != NULL)
+//	{
+//		Node* p = createNode(x);
+//
+//		p->next = q->next; // gan phan tu tiep theo cua q bang phan tu tiep theo cua p
+//		q->next->prev = p; // phan tu ke tiep cua q tro ve truoc gan bang p;
+//
+//		q->next = p;	   
+//		p->prev = q;	   // p tro toi prev bang q
+//	}
+//}
+//void xoaSau(ds_Kep& k, int v)
+//{
+//	Node* q = k.head;
+//	while (q != NULL && q->value != v)
+//		q = q->next;
+//
+//		 
+//		Node* p = q->next;
+//		q->next = p->next;
+//		if (p->next != NULL)
+//		{
+//			p->next->prev = q;
+//		}
+//		delete p;
+//}
+//void xoaDau(ds_Kep& k)
+//{
+//	if (k.head != NULL)
+//	{
+//		Node* p = k.head;
+//		if (p->next == NULL)
+//		{
+//			k.tail = NULL;
+//			k.head = NULL;
+//		}
+//		else
+//		{
+//			k.head = p->next;  // cap nhat gia tri cua bien head thanh phan tu tiep theo
+//			p->next->prev = NULL; // phan tu ke phan tu head tro ve dau bang NULL
+//			p->next = NULL;		  
+//		}
+//		delete(p);
+//	}
+//}
+//void xoaCuoi(ds_Kep& k)
+//{
+//	if (k.head != NULL)
+//	{
+//		Node* p = k.tail;
+//		k.tail = p->prev;
+//		p->next = NULL;
+//		p->prev->next = NULL;
+//
+//		delete(p);
+//	}
+//}
+//
+//Node* timKiem(ds_Kep &k, int v)
+//{
+//	Node* p = k.head;
+//	while (p != NULL && p->value != v)
+//	{
+//		p = p->next;
+//	}
+//	return p;
+//	
+//}
+//void output(ds_Kep k)
+//{
+//	Node* p = k.head;
+//	while (p!=NULL)
+//	{
+//		cout << p->value << "\t";
+//		p = p->next;
+//	}
+//}
+//void output_1(ds_Kep k)
+//{
+//	Node* p = k.tail;
+//	while (p!=NULL)
+//	{
+//		cout << p->value << "\t";
+//		p = p->prev;
+//	}
+//}
+//int main()
+//{
+//	ds_Kep k;
+//	init(k);
+//	themCuoi(k, 10);
+//	themDau(k, 20);
+//	themDau(k, 30);
+//	themCuoi(k, 50);
+//	themsau(k, 30, 31); // 30 31 20 10 50
+//	xoaDau(k); // 31 20 10 50
+//	xoaCuoi(k); // 31 20 10
+//	xoaSau(k, 10); // 31 20s
+//	output(k);
+//	cout << endl;
+//	if (timKiem(k, 110) != NULL)
+//		cout << "Tim Thay";
+//	else
+//	{
+//		cout << "Khong tim thay";
+//	}
+//	return 0;
+//}
